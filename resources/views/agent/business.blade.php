ff0c875034d790dec28406ca1067e8e9707e470a
shiva <ashishsapra7@gmail.com>
2018-12-25 14:42:33 +0530

    frontend - agent 2

--- resources/views/agent/business.blade.php
+++ resources/views/agent/business.blade.php
@@ -8,9 +8,11 @@
           <div class="col-12">
             <div class="card">
               <div class="card-content collapse show">
-                <div class="row">
+            <div class="row">
+
                 <div class="col-xl-4 col-lg-6 col-md-12 border-right-blue-grey border-right-lighten-5">
                     <div class="my-1 text-center">
+                        <a href="{{route('studentList',['id'=>$agent->id])}}" style="text-decoration: none;">
                       <div class="card-header mb-2 pt-0">
                         <h5 class="warning">No. of students</h5>
                         <h3 class="font-large-2 text-bold-200">{{$agent->students}}
@@ -22,14 +24,16 @@
                         data-height="130" data-inputColor="#BABFC7" data-readOnly="true"
                         data-fgColor="#FFA87D" data-knob-icon="icon-energy">
                       </div>
+                  </a>
                     </div>
                 </div>
 
                 <div class="col-xl-4 col-lg-6 col-md-12 border-right-blue-grey border-right-lighten-5">
                     <div class="my-1 text-center">
+                        <a href="{{route('agent.contracts',['id'=>$agent->id])}}" style="text-decoration: none;">
                       <div class="card-header mb-2 pt-0">
-                        <h5 class="danger">Files Not Started</h5>
-                        <h3 class="font-large-2 text-bold-200">{{$files_not_started}}
+                        <h5 class="danger">Total Contracts</h5>
+                        <h3 class="font-large-2 text-bold-200">{{$agent->contracts}}
                         </h3>
                       </div>
                       <div class="card-content">
@@ -38,15 +42,17 @@
                         data-height="130" data-inputColor="#BABFC7" data-readOnly="true"
                         data-fgColor="#FF7588" data-knob-icon="icon-pointer">
                       </div>
+                  </a>
                     </div>
                 </div>
 
                         <div class="col-xl-4 col-lg-6 col-md-12 border-right-blue-grey border-right-lighten-5">
                     
                     <div class="my-1 text-center">
+                        <a href="#" style="text-decoration: none;">
                       <div class="card-header mb-2 pt-0">
-                        <h5 class="primary">Files in Process</h5>
-                        <h3 class="font-large-2 text-bold-200">{{$files_in_process}}</h3>
+                        <h5 class="primary">Total revenue</h5>
+                        <h3 class="font-large-2 text-bold-200">{{$agent->revenue}}</h3>
                       </div>
                       <div class="card-content">
                         <input type="text" value="65" class="knob hide-value responsive angle-offset" data-angleOffset="40"
@@ -54,61 +60,50 @@
                         data-height="130" data-inputColor="#BABFC7" data-readOnly="true"
                         data-fgColor="#00B5B8" data-knob-icon="icon-trophy">
                       </div>
+                  </a>
                     </div> 
                 </div>
-        </div>
-        		<div class="row">
-                    <div class="col-lg-3">
-                        <a href="{{route('studentList',['id'=>$agent->id])}}" style="text-decoration: none;">
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                				<div class="card-header text-center">No. of students</div>
-                                <div class="card-body bg-light text-dark">
-                                    <h3 class="card-title text-center">{{$agent->students}}</h3>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-3">
-                        <a href="{{route('agent.contracts',['id'=>$agent->id])}}" style="text-decoration: none;">
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                				<div class="card-header text-center">Total Contracts</div>
-                                <div class="card-body bg-light text-dark">
-                                    <h3 class="card-title text-center">{{$agent->contracts}}</h3>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-3">
+            </div>
+        		
+                <div class="row">
+
+                <div class="col-xl-4 col-lg-6 col-md-12 border-right-blue-grey border-right-lighten-5">
+                    <div class="my-1 text-center">
                         <a href="#" style="text-decoration: none;">
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                				<div class="card-header text-center">Total revenue</div>
-                                <div class="card-body bg-light text-dark">
-                                    <h3 class="card-title text-center">{{$agent->revenue}}</h3>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-3">
-                        <a href="#" style="text-decoration: none;">
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                				<div class="card-header text-center">Commission</div>
-                                <div class="card-body bg-light text-dark">
-                                    <h3 class="card-title text-center">{{$agent->commission}}</h3>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-3">
-                        <a href="{{route('agent.files',['id'=>$agent->id])}}" style="text-decoration: none;">
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                                <div class="card-header text-center">Total files</div>
-                                <div class="card-body bg-light text-dark">
-                                    <h3 class="card-title text-center">{{$agent->total_files}}</h3>
-                                </div>
-                            </div>
-                        </a>
+                      <div class="card-header mb-2 pt-0">
+                        <h5 class="warning">Commission</h5>
+                        <h3 class="font-large-2 text-bold-200">{{$agent->commission}}
+                        </h3>
+                      </div>
+                      <div class="card-content">
+                        <input type="text" value="81" class="knob hide-value responsive angle-offset" data-angleOffset="20"
+                        data-thickness=".15" data-linecap="round" data-width="130"
+                        data-height="130" data-inputColor="#BABFC7" data-readOnly="true"
+                        data-fgColor="#FFA87D" data-knob-icon="icon-energy">
+                      </div>
+                  </a>
                     </div>
                 </div>
+
+                <div class="col-xl-4 col-lg-6 col-md-12 border-right-blue-grey border-right-lighten-5">
+                    <div class="my-1 text-center">
+                        <a href="{{route('agent.files',['id'=>$agent->id])}}" style="text-decoration: none;">
+                      <div class="card-header mb-2 pt-0">
+                        <h5 class="danger">Total files</h5>
+                        <h3 class="font-large-2 text-bold-200">{{$agent->total_files}}
+                        </h3>
+                      </div>
+                      <div class="card-content">
+                        <input type="text" value="70" class="knob hide-value responsive angle-offset" data-angleOffset="0"
+                        data-thickness=".15" data-linecap="round" data-width="130"
+                        data-height="130" data-inputColor="#BABFC7" data-readOnly="true"
+                        data-fgColor="#FF7588" data-knob-icon="icon-pointer">
+                      </div>
+                  </a>
+                    </div>
+                </div>
+                
+                
             </div>
         </div>
     </div>
--- resources/views/agent/studentList.blade.php
+++ resources/views/agent/studentList.blade.php
@@ -1,48 +1,85 @@
 @extends("layouts.frontend")
+@section('css')
+	<link rel="stylesheet" type="text/css" href="{{asset("app/front/app-assets/css/vendors.css")}}">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN STACK CSS-->
+  <link rel="stylesheet" type="text/css" href="{{asset("app/front/app-assets/css/app.css")}}">
+  <!-- END STACK CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="{{asset("app/front/app-assets/css/core/menu/menu-types/vertical-menu.css")}}">
+  <link rel="stylesheet" type="text/css" href="{{asset("app/front/app-assets/css/core/colors/palette-callout.css")}}">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="{{asset("app/front/assets/css/style.css")}}">
+  <!-- END Custom CSS-->
+@endsection
 @section('content')
-	<div class="card-header text-center"><strong>Students under "{{$agent->name}}"</strong></div>
-	<div class="card-body">
-		
-		<table class="table table-hover">
-			<thead>
-				<th>
-					First Name
-				</th>
-				<th>
-					Last Name
-				</th>
-				<th>
-					Email
-				</th>
-				<th>
-					Gender
-				</th>
-				
-			</thead>
-			<tbody>
-				@if($students->count()>0)
-					@foreach($students as $student)
-					<tr>
-						<td><a href="{{route('student.details',['id'=>$student->id])}}">
-							{{$student->first_name}}</a>
-						</td>
-						<td>{{$student->last_name}}</td>
-						<td>{{$student->email}}</td>
-						<td>{{$student->gender}}</td>
-						{{-- <td>
-							<a href="{{route('post.edit',['id' => $post->id])}}" class="btn btn-sm btn-info">Edit</a>
-							<a href="{{route('post.delete',['id' => $post->id])}}" class="btn btn-sm btn-danger">Trash</a>
-						</td> --}}
-					</tr>
-					@endforeach
-				
-				@else
-					<tr>
-						<th colspan="5" class="text-center">No Students!!</th>
-					</tr>
-				@endif
+<div class="content-header text-center">
+    	<h3 class="content-header-title"><strong>Students under "{{$agent->name}}"</strong></h3>
+    </div>
+	<div class="content-body">
+		<div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-content collapse show">
+				<div class="table-responsive">
+				<table class="table table-hover mb-0">
+					<thead>
+						<th>#</th>
+						<th>
+							First Name
+						</th>
+						<th>
+							Last Name
+						</th>
+						<th>
+							Email
+						</th>
+						<th>
+							Gender
+						</th>
+						
+					</thead>
+					<tbody>
+						@if($students->count()>0)
+							@foreach($students as $student)
+							<tr><th scope="row">{{$student->id}}</th>
+								<td><a href="{{route('student.details',['id'=>$student->id])}}">
+									{{$student->first_name}}</a>
+								</td>
+								<td>{{$student->last_name}}</td>
+								<td>{{$student->email}}</td>
+								<td>{{$student->gender}}</td>
+							</tr>
+							@endforeach
+						
+						@else
+							<tr>
+								<th colspan="5" class="text-center">No Students!!</th>
+							</tr>
+						@endif
 
-			</tbody>
-		</table>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>
+	</div>
+	</div>
+	</div>
+
+	
 @stop
+@section('js')
+	<script src="{{asset("app/front/app-assets/vendors/js/vendors.min.js")}}" type="text/javascript"></script>
+  <!-- BEGIN VENDOR JS-->
+  <!-- BEGIN PAGE VENDOR JS-->
+  <!-- END PAGE VENDOR JS-->
+  <!-- BEGIN STACK JS-->
+  <script src="{{asset("app/front/app-assets/js/core/app-menu.js")}}" type="text/javascript"></script>
+  <script src="{{asset("app/front/app-assets/js/core/app.js")}}" type="text/javascript"></script>
+  <script src="{{asset("app/front/app-assets/js/scripts/customizer.js")}}" type="text/javascript"></script>
+  <!-- END STACK JS-->
+  <!-- BEGIN PAGE LEVEL JS-->
+  <!-- END PAGE LEVEL JS-->
+@endsection
