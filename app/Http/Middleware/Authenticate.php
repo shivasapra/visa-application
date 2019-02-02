d4a7a625aaa694db06a9e7ce362484f3d701231f
shiva <ashishsapra7@gmail.com>
2019-02-02 23:52:09 +0530

    Session

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use Session;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,6 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            Session::flash('info','Please login first ');
             return route('login');
         }
     }
